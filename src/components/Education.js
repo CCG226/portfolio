d7fa404f73cd1174a3b9bbf832e582276bd16bce
CCG226 <cown877@gmail.com>
2025-04-27 17:49:39 -0500

    edu completed

--- src/components/Education.js
+++ src/components/Education.js
@@ -12,19 +12,16 @@ function Education() {
         <li className="fs-3 text-start">
           <span class="material-symbols-outlined test">workspace_premium</span>{" "}
           BS in Computer Science (Dec 2024)
-          <div className="text-center msg">*In Progress</div>
         </li>
         <div>&nbsp;</div>
         <li className="fs-3 text-start">
           <span class="material-symbols-outlined test">workspace_premium</span>{" "}
           BS in Computer Technology (Dec 2024)
-          <div className="text-center msg">*In Progress</div>
         </li>
         <div>&nbsp;</div>
         <li className="fs-3 text-start">
           <span class="material-symbols-outlined test">workspace_premium</span>{" "}
           Certificate in Internet and Web (Dec 2024)
-          <div className="text-center msg">*In Progress</div>
         </li>
       </ul>
     </div>
